163cb62903a74ef63e1ce48ea60732368b16deb4
emprexvest <121636284+emprexvest@users.noreply.github.com>
2024-11-15 12:02:32 -0800

    Add files via upload

--- finalproject/src/main.c
+++ finalproject/src/main.c
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-// #include "common.h"
-// #include "file.h"
-// #include "parse.h"
+#include "common.h"
+#include "file.h"
+#include "parse.h"
 
 void print_usage(char *argv[]) {
     printf("Usage: %s -n -f <database file>\n", argv[0]);
@@ -20,6 +20,7 @@ int main(int argc, char *argv[]) {
 	char *filepath = NULL;
     bool newfile = false;
     int c;
+    int dbfd = -1;
 
     while((c = getopt(argc, argv, "nf:")) != -1) {
         switch(c) {
@@ -41,6 +42,25 @@ int main(int argc, char *argv[]) {
         printf("Filepath is a required argument\n");
         print_usage(argv);
     }
+
+    if(newfile) {
+        dbfd = create_db_file(filepath);
+        if(dbfd == STATUS_ERROR) {
+            printf("Unable to create database file\n");
+            return -1;
+        }
+    } else {
+        dbfd = open_db_file(filepath);
+        if(dbfd == STATUS_ERROR) {
+            printf("Unable to open database file\n");
+            return -1;
+        }
+    }
+
+
+
+
+
 
     printf("Newfile: %d\n", newfile);
     printf("Filepath: %s\n", filepath);
